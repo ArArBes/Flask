0197e5a3c3bf52d870018a51c11a99b4c799b558
ArBes <arina.arifovnaa@mail.com>
2025-07-30 13:47:48 +0300

    firstLesson

---
+++ Flask_Урок 2/app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+
+@app.route('/hello')
+def hello_world():
+    return "Hello, World!"
+
+@app.route('/info')
+def info():
+    return "This is an informational page."
+
+@app.route('/calc/<num1>/<num2>')
+def calc(num1, num2):
+    try:
+        n1 = int(num1)
+        n2 = int(num2)
+    except ValueError:
+        return "Incorrect data", 400
+    sum_ = n1 + n2
+    return f'The sum of {n1} and {n2} is {sum_}'
+
+@app.route('/reverse/<string:str>')
+def reverse(str):
+    if str == '':
+        return 'Incorrect data', 400
+    return f'{str[::-1]}'
+
+@app.route('/user/<name>/<age>')
+def user(name, age):
+    try:
+        age_int = int(age)
+        if age_int < 0:
+            return "Incorrect data", 400
+    except ValueError:
+        return "Incorrect data", 400
+    return f'Hello, {name}. You are {age_int} years old.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- project/app.py
+++ project/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from apps import routes
+print(app.url_map)
+
 @app.route('/')
 def hello():
     return "Hello, Flask!"
